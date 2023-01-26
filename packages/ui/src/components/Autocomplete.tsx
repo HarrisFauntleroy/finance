caf37e26fc578dbbfe117ccea3a02e480bb52e6c
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2023-01-26 17:12:25 +1000

    test

--- packages/ui/src/components/Autocomplete.tsx
+++ packages/ui/src/components/Autocomplete.tsx
@@ -20,6 +20,7 @@ import { Controller, useFormContext } from "react-hook-form"
 interface AutocompleteProps {
 	label: string
 	name: string
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	data: any[]
 	error?: string
 }
