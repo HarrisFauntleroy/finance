34a8b5cd02e550f706da9b4d34c809f2f8b8e564
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2023-05-25 20:26:56 +1000

    * feat(_app.tsx): remove SessionProvider and DefaultSeo components from MyApp and add them to AppContext * chore(_app.tsx): move NextPageWithLayout type definition to the bottom of the file

--- apps/web/components/Providers/index.tsx
+++ apps/web/components/Providers/index.tsx
@@ -1,5 +1,7 @@
-import { Fragment, PropsWithChildren } from "react";
-
+import { SessionProvider } from "next-auth/react";
+import { DefaultSeo } from "next-seo";
+import { type PropsWithChildren } from "react";
+import SEO from "../../next-seo.config";
 import { AppPropsWithLayout } from "../../pages/_app";
 import { IntlProvider } from "../Providers/I18n";
 import { PrivacyProvider } from "../Providers/Privacy";
@@ -9,13 +11,18 @@ export function AppContext<T>({
   children,
   ...props
 }: AppPropsWithLayout & PropsWithChildren<T>) {
+  const {
+    pageProps: { session },
+  } = props;
+
   return (
-    <Fragment>
+    <SessionProvider session={session}>
+      <DefaultSeo {...SEO} />
       <ThemeProvider {...props}>
         <PrivacyProvider>
           <IntlProvider>{children}</IntlProvider>
         </PrivacyProvider>
       </ThemeProvider>
-    </Fragment>
+    </SessionProvider>
   );
 }
--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,21 +1,35 @@
-import { ReactElement, ReactNode } from "react";
-
-import NextApp, {
-  type AppProps,
-  type AppContext as NextJsAppContext,
-} from "next/app";
-import { initI18n } from "../i18n";
-import SEO from "../next-seo.config";
-import { trpc } from "../utils/trpc";
-
 import { ColorScheme } from "@mantine/core";
 import { getCookie } from "cookies-next";
 import type { Role } from "database/generated/prisma-client";
 import { type NextPage } from "next";
 import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { DefaultSeo } from "next-seo";
+import NextApp, {
+  type AppProps,
+  type AppContext as NextJsAppContext,
+} from "next/app";
+import { ReactElement, ReactNode } from "react";
 import { AppContext } from "../components/Providers";
+import { initI18n } from "../i18n";
+import { trpc } from "../utils/trpc";
+
+function MyApp(props: AppPropsWithLayout) {
+  const { Component, pageProps } = props;
+  initI18n();
+
+  return (
+    <AppContext {...props}>
+      <Component {...pageProps} />
+    </AppContext>
+  );
+}
+
+MyApp.getInitialProps = async (appContext: NextJsAppContext) => {
+  const appProps = await NextApp.getInitialProps(appContext);
+  return {
+    ...appProps,
+    colorScheme: getCookie("mantine-color-scheme", appContext.ctx) || "light",
+  };
+};
 
 type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<
   P,
@@ -29,28 +43,6 @@ type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<
 export type AppPropsWithLayout = AppProps<{ session: Session | null }> & {
   Component: NextPageWithLayout;
   colorScheme: ColorScheme;
-};
-
-function MyApp(props: AppPropsWithLayout) {
-  const { Component, pageProps } = props;
-  initI18n();
-
-  return (
-    <SessionProvider session={pageProps.session}>
-      <DefaultSeo {...SEO} />
-      <AppContext {...props}>
-        <Component {...pageProps} />
-      </AppContext>
-    </SessionProvider>
-  );
-}
-
-MyApp.getInitialProps = async (appContext: NextJsAppContext) => {
-  const appProps = await NextApp.getInitialProps(appContext);
-  return {
-    ...appProps,
-    colorScheme: getCookie("mantine-color-scheme", appContext.ctx) || "light",
-  };
 };
 
 export default trpc.withTRPC(MyApp);
