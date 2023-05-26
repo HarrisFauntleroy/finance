0d5f5200ba29afefb8aea67653952f622a7dcea5
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2023-05-26 13:50:35 +1000

    * refactor(auth): extract CenterMessage component to reduce code duplication * feat(home): add auth property to HomePage component * chore(markets): reorder imports and remove unused import statements

--- apps/web/pages/auth/index.tsx
+++ apps/web/pages/auth/index.tsx
@@ -1,4 +1,4 @@
-import { Center, Progress, Stack, Text } from "@chakra-ui/react";
+import { Center, Text } from "@chakra-ui/react";
 import { Role } from "database/generated/prisma-client";
 import type { NextPageContext } from "next";
 import { getSession, useSession } from "next-auth/react";
@@ -10,6 +10,7 @@ type AuthProps<T> = PropsWithChildren<T> & {
 
 export const getServerSideProps = async (context: NextPageContext) => {
   const session = await getSession(context);
+
   if (!session) {
     return {
       redirect: {
@@ -30,32 +31,23 @@ function Auth<T>({ children, roles }: AuthProps<T>) {
   const rolesArray = roles || [Role.USER];
   const roleAllowed = rolesArray.includes(userRole) || userRole === Role.ADMIN;
 
+  const CenterMessage = ({ message }: { message: string }) => (
+    <Center height="100%">
+      <Text>{message}</Text>
+    </Center>
+  );
+
   if (status === "loading") {
-    return (
-      <Center height="100%">
-        <Stack>
-          <Text>Loading...</Text>
-          <Progress isIndeterminate />
-        </Stack>
-      </Center>
-    );
+    return <CenterMessage message="Loading..." />;
   }
 
   if (!session) {
-    return (
-      <Center height="100%">
-        <Text>You must be logged in to view this page.</Text>
-      </Center>
-    );
+    return <CenterMessage message="You must be logged in to view this page." />;
   }
 
   if (!roleAllowed) {
     return (
-      <Center height="100%">
-        <Text>
-          You do not have the necessary permissions to view this page.
-        </Text>
-      </Center>
+      <CenterMessage message="You do not have the necessary permissions to view this page." />
     );
   }
 
