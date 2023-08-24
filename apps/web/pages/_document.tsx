d591145b3a31ce852676cfb967034e7d334461da
HarrisFauntleroy <harrisfauntleroy@gmail.com>
2023-08-24 21:53:02 +1000

    📦 chore(package.json): update mantine/core, mantine/hooks, and mantine/next to v6.0.19 ➕ feat(package.json): add @emotion/server to dependencies for server-side rendering support 🔧 refactor(_document.tsx): replace simple Document function with a class component to support server-side rendering with mantine/next and @emotion/server

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -1,33 +1,58 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyles, createStylesServer } from "@mantine/next";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
 
-function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+const stylesServer = createStylesServer();
+
+export default class _Document extends Document {
+  static async getInitialProps(context: DocumentContext) {
+    const initialProperties = await Document.getInitialProps(context);
+
+    return {
+      ...initialProperties,
+      styles: [
+        initialProperties.styles,
+        <ServerStyles
+          html={initialProperties.html}
+          server={stylesServer}
+          key="styles"
+        />,
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
-
-export default Document;
